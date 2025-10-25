38e832add3dbce10f5f00736e9ef50b18b2a0b0c
dsadoski <115582410+dsadoski@users.noreply.github.com>
2025-10-24 20:30:05 -0400

    Empty Project

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SailMonitor")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SailMonitor.Pages")]
